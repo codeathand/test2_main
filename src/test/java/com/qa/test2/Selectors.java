7684da7e892ddf76bb93fe39899eb743250b3c47
BCEvaPI1974@#DJ <codeathand@gmail.com>
2021-03-04 09:13:11 +0100

    changes add to watchlist 4

--- src/test/java/com/qa/test2/Selectors.java
+++ src/test/java/com/qa/test2/Selectors.java
@@ -187,9 +187,10 @@ public class Selectors {
 		// enter volume range and submit
 		driver.findElement(volumeFrom).sendKeys("1");
 		driver.findElement(volumeTo).sendKeys("45000");
+		Thread.sleep(2000);
 		WebDriverWait wait1 = new WebDriverWait(driver, timeoutInSeconds);
 		wait1.until(ExpectedConditions.elementToBeClickable(applyVolume));
-		Thread.sleep(2000);
+
 		driver.findElement(applyVolume).click();
 		Thread.sleep(2000);
 		// validate that filtered table has less than 5 rows
@@ -218,7 +219,7 @@ public class Selectors {
 		WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
 		wait.until(ExpectedConditions.elementToBeClickable(addToWatchList));
 		// when click on the star to add to watch list, check whether is modal window is displayed
-//		Thread.sleep(3000);
+
 		driver.findElement(addToWatchList).click();
 		Thread.sleep(2000);
 		WebDriverWait wait1 = new WebDriverWait(driver, timeoutInSeconds);
